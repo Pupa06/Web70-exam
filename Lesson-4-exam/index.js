8dbb58c81f07e135f400087baa23dc82fa0e684d
Pupa06 <tduy8901@gmail.com>
2023-08-02 21:29:16 +0700

    update lesson-4-exam

--- Lesson-4-exam/index.js
+++ Lesson-4-exam/index.js
@@ -4,8 +4,8 @@ import router from "./src/routers/index.js";
 const app = express();
 const PORT = 5000;
 
-app.use("/api" , router);
 app.use(express.json())
+app.use("/api" , router);
 
 app.listen(PORT, () => {
     console.log("Server listening on port" + PORT);
--- Lesson-4-exam/src/middlewares/validateApiKey.js
+++ Lesson-4-exam/src/middlewares/validateApiKey.js
@@ -1,16 +1,34 @@
-import { users } from "../data/users";
+import { users } from "../data/users.js";
 
 export const validateApiKey = (req, res, next) => {
-    const apiKey = req.query.apiKey;
-    if (!apiKey) {
-        return res.status(403).json({ message: "Không được phép" });
+    const { apikey, username } = req.query;
+
+    if (!apikey && !username) {
+        return res.status(401).json({ message: "Không được phép" });
     }
 
-    const user = users.find((user) => user.apiKey === apiKey);
-    if (!user) {
-        return res.status(403).json({ message: "Không được phép" });
+    if (apikey) {
+        // Kiểm tra apiKey có hợp lệ không bằng cách so sánh với dữ liệu users
+        const userWithApiKey = users.find((user) => user.apiKey === apikey);
+
+        if (!userWithApiKey) {
+            return res.status(401).json({ message: "Không được phép" });
+        }
     }
 
-    req.user = user;
+    if (username) {
+        // Kiểm tra username có hợp lệ không bằng cách so sánh với dữ liệu users
+        const userWithUsername = users.find((user) => user.username === username);
+
+        if (!userWithUsername) {
+            return res.status(401).json({ message: "Không được phép" });
+        }
+    }
+
+    // Lưu thông tin người dùng vào req.user để sử dụng ở các API khác
+    req.user = {
+        id: username ? users.find((user) => user.username === username).id : null,
+    };
+
     next();
-}
+};
